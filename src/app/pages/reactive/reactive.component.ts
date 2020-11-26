2b11d11db1037ad1139b8bb3fd0b44ce05192861
irwinet <irwinestradatorres@gmail.com>
2020-11-26 18:23:13 -0500

    Add Validators Service And Validator Sincrono

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidadoresService } from '../../services/validadores.service';
 
 @Component({
   selector: 'app-reactive',
@@ -10,7 +11,8 @@ export class ReactiveComponent implements OnInit {
 
   forma: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+              private validadores: ValidadoresService) {
     this.crearFormulario();
     this.cargarDataAlFormulario();
   }
@@ -42,16 +44,31 @@ export class ReactiveComponent implements OnInit {
     return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched;
   }
 
+  get pass1NoValido(){
+    return this.forma.get('pass1').invalid && this.forma.get('pass1').touched;
+  }
+
+  get pass2NoValido(){
+    const pass1 = this.forma.get('pass1').value;
+    const pass2 = this.forma.get('pass2').value;
+
+    return (pass1 === pass2) ? false : true;
+  }
+
   crearFormulario(){
     this.forma = this.fb.group({
       nombre  : ['', [Validators.required, Validators.minLength(5)]],
-      apellido: ['', [Validators.required, Validators.minLength(5)]],
+      apellido: ['', [Validators.required, Validators.minLength(5), this.validadores.noEstrada]],
       correo  : ['', [Validators.required ,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      pass1 : ['', Validators.required],
+      pass2 : ['', Validators.required],
       direccion: this.fb.group({
         distrito: ['', Validators.required],
         ciudad: ['', Validators.required]
       }),
       pasatiempos: this.fb.array([])
+    },{
+      validators: this.validadores.passwordsIguales('pass1','pass2')
     });
   }
 
---
+++ src/app/services/validadores.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidadoresService {
+
+  constructor() { }
+
+  noEstrada(control: FormControl): { [s:string]: boolean }{
+    if(control.value?.toLowerCase()=='estrada'){
+      return {
+        noEstrada : true
+      }
+    }
+    
+    return null;
+  }
+
+  passwordsIguales(pass1Name: string, pass2Name: string){
+    return (formGroup: FormGroup) => {    
+      const pass1Control = formGroup.controls[pass1Name];
+      const pass2Control = formGroup.controls[pass2Name];
+
+      if(pass1Control.value === pass2Control.value){
+        pass2Control.setErrors(null);
+      }
+      else{
+        pass2Control.setErrors({noEsIgual: true});
+      }
+    }
+  }
+}
